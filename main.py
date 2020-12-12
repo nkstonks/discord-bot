fba79e28a13d523e8a0730dd8bd5d273738d7519
Kento <nkento2007@gmail.com>
2020-12-12 02:09:28 +0000

    bugfix for token

--- main.py
+++ main.py
@@ -7,16 +7,18 @@ import random
 
 bot = commands.Bot(command_prefix="c.", help_command=None)
 
+
 @bot.event
 async def on_ready():
-    activity = discord.Activity(type=discord.ActivityType.watching, name="for c.help")
+    activity = discord.Activity(
+        type=discord.ActivityType.watching, name="for c.help")
 
     await bot.change_presence(activity=activity)
     print(f'{bot.user.name} has connected to Discord!')
 
+
 @bot.command(name="help")
 async def help(ctx):
-    
     """Gives the list of commands"""
 
     response = dedent("""
@@ -30,9 +32,9 @@ async def help(ctx):
 
     await ctx.send(response)
 
+
 @bot.command(name="nsfw-off")
 async def nsfw_off(ctx):
-
     """Toggles nsfw content off"""
 
     message = dedent("```NSFW content is turned off for the whole server.```")
@@ -40,9 +42,9 @@ async def nsfw_off(ctx):
     nsfw = False
     await ctx.send(message)
 
+
 @bot.command(name="nsfw-on")
 async def nsfw_on(ctx):
-
     """Toggles nsfw content on"""
 
     message = dedent("```NSFW content is turned on for the whole server.```")
@@ -50,12 +52,11 @@ async def nsfw_on(ctx):
     nsfw = True
     await ctx.send(message)
 
+
 @tasks.loop(hours=3)
 async def remind():
-
     """Reminds a cursed fact"""
 
-    
     thing1 = "```Reminder:\nA whales' penis is 3m long, and 30cm in diameter.```"
     thing2 = "```Reminder:\nRupunzel, Rupunzel, let down your pubic hair!\nYes I needed to say that. :D```"
     thing3 = "```Reminder:\nYour parents did a special dance in front of god, and god gave them a baby.```"
@@ -64,16 +65,15 @@ async def remind():
     thing6 = "```Reminder:\nCheck out the tricky jar story: https://cursedreminder.herokuapp.com/the_story```"
     thing7 = "```Reminder:\nYou parents did sex while you were alive, if you have younger brothers/sisters.```"
 
-    number = "thing" + str(random.randint(1,7))
+    number = "thing" + str(random.randint(1, 7))
 
-    
     response = eval(number)
 
     text_channel_list = []
     for channel in bot.get_all_channels():
         # print(channel)
         text_channel_list.append(channel)
-    
+
     # print(text_channel_list)
 
     for channel in text_channel_list:
@@ -81,18 +81,19 @@ async def remind():
         # print(channel_name)
         try:
             await channel_name.send(response)
-        
+
         except AttributeError:
             continue
-    
+
 
 @remind.before_loop
 async def before():
     await bot.wait_until_ready()
 
+
 remind.start()
 keep_alive.keep_alive()
 
-token = getenv(DISCORD_TOKEN)
+token = getenv("DISCORD_TOKEN")
 
 bot.run(token)
